15a472074e2be08dee193608f8a27d7563115dd3
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-02-03 22:29:52 +0200

    конец 6 дз

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,5 +2,9 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
+  if (event.currentTarget.value === "") {
+    output.textContent = "Anonymous";
+    return;
+  }
   output.textContent = event.currentTarget.value;
 });
